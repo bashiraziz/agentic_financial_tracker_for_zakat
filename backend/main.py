87e988c37cf2c3e29dbdf817d45f3a7436dcc4e4
Bashir Aziz <bashiraziz@users.noreply.github.com>
2025-10-28 19:28:12 -0400

    modified main

--- backend/main.py
+++ backend/main.py
@@ -72,9 +72,9 @@ async def health_check():
     return {"status": "ok", "message": "Backend is reachable from Vercel"}
 
 # added the below to fix the health check issue
-import os
-import uvicorn
+# import os
+# import uvicorn
 
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     port = int(os.getenv("PORT", 8000))
+#     uvicorn.run("backend.main:app", host="0.0.0.0", port=port)
